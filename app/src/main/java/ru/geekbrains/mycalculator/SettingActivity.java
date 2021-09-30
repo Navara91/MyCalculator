534947cf645f083679ebb744ad0992a2c92b3b30
Nikolay <kuoluska@mail.ru>
2021-10-01 01:25:43 +0900

    setonclicklistener for switch

--- app/src/main/java/ru/geekbrains/mycalculator/SettingActivity.java
+++ app/src/main/java/ru/geekbrains/mycalculator/SettingActivity.java
@@ -3,6 +3,7 @@ package ru.geekbrains.mycalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Switch;
 
 public class SettingActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class SettingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_setting);
+
+        Switch switchTheme = findViewById(R.id.switchChangeTheme);
+        if (switchTheme.isChecked()){
+
+        } else {
+
+        }
     }
 }
